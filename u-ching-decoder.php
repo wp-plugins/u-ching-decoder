988c1ad042cc6e68cf504849e58970e44d44836f
tonyray <tonyray@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-08 10:02:40 +0000

    Mods to sense post type: event
    git-svn-id: https://plugins.svn.wordpress.org/u-ching-decoder/trunk@1108001 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- u-ching-decoder.php
+++ u-ching-decoder.php
@@ -1,29 +1,29 @@
-<?php
-/*
-Plugin Name: u-Ching Decoder
-Plugin URI: http://anthonyfogleman.com/blog/u-ching-decoder-wp-plugin/
-Description: The u-Ching is a new application for the Dreamspell. With this tool you are able to decode the ancient 64 hexagrams of the I Ching into their corresponding number frequency, not known before. This re-orders all the chapters of the I into U. In this ancient new knowledge we continue our study of the Genetic Code in Time. Every hexagram (or codon) is related with one of 64 UR Runes, we learned about in the Law of Time. The u-Ching became available as a gift from the People of O.M.A. around Galactic Synchronization in 2013. This Wordpress plugin works as a widget you can drop in a sidebar. It is responsive and resizes to certain proportions with theme, for example on mobile devices.
-Version: 1.1.57
-Author: Juryt Abma and Anthony R. Fogleman
-Author URI: http://anthonyfogleman.com
-License: GPLv2
-*/
-
-/*  Copyright 2013  Anthony R. Fogleman  ( circle@uptimehosting.com )
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+<?php
+/*
+Plugin Name: u-Ching Decoder
+Plugin URI: http://anthonyfogleman.com/blog/u-ching-decoder-wp-plugin/
+Description: The u-Ching is a new application for the Dreamspell. With this tool you are able to decode the ancient 64 hexagrams of the I Ching into their corresponding number frequency, not known before. This re-orders all the chapters of the I into U. In this ancient new knowledge we continue our study of the Genetic Code in Time. Every hexagram (or codon) is related with one of 64 UR Runes, we learned about in the Law of Time. The u-Ching became available as a gift from the People of O.M.A. around Galactic Synchronization in 2013. This Wordpress plugin works as a widget you can drop in a sidebar. It is responsive and resizes to certain proportions with theme, for example on mobile devices.
+Version: 1.1.57
+Author: Juryt Abma and Anthony R. Fogleman
+Author URI: http://anthonyfogleman.com
+License: GPLv2
+*/
+
+/*  Copyright 2013  Anthony R. Fogleman  ( circle@uptimehosting.com )
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 /**
  * Proper way to enqueue scripts and styles
@@ -57,10 +57,10 @@ add_filter('widget_text', 'do_shortcode');
 include('ucd_construct.inc');
 
 // Make a shortcode for full-page use
-add_shortcode('u-ching-display', 'start_uching');
+add_shortcode('u-ching-display', 'start_uching');
 
 // Include wp menu and settings
-include('settings.inc');
+include('settings.inc');
 
 // create custom plugin settings
 add_action('admin_menu', 'ucd_admin_options_menu');
